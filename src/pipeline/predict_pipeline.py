dc7a0112286f51985ddc6e300e4f33ee7a245aa8
Prashant <prashantverma813@gmail.com>
2025-04-19 05:52:26 +0530

    now its working and pridiciting

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,8 +1,8 @@
 import sys
 import pandas as pd
+import os
 from src.exception import CustomException
 from src.utils import load_object
-
 
 class PredictPipeline:
     def __init__(self):
@@ -19,34 +19,17 @@ class PredictPipeline:
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
             return preds
-        
         except Exception as e:
             raise CustomException(e,sys)
 
-
-
 class CustomData:
-    def __init__(  self,
-        gender: str,
-        race_ethnicity: str,
-        parental_level_of_education,
-        lunch: str,
-        test_preparation_course: str,
-        reading_score: int,
-        writing_score: int):
-
+    def __init__(self, gender: str, race_ethnicity: str, parental_level_of_education, lunch: str, test_preparation_course: str, reading_score: int, writing_score: int):
         self.gender = gender
-
         self.race_ethnicity = race_ethnicity
-
         self.parental_level_of_education = parental_level_of_education
-
         self.lunch = lunch
-
         self.test_preparation_course = test_preparation_course
-
         self.reading_score = reading_score
-
         self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
@@ -60,8 +43,6 @@ class CustomData:
                 "reading_score": [self.reading_score],
                 "writing_score": [self.writing_score],
             }
-
             return pd.DataFrame(custom_data_input_dict)
-
         except Exception as e:
             raise CustomException(e, sys)
